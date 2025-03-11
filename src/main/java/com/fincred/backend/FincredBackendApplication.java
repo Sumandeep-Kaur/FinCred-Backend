f8427608580895b86c2dea2d80a569387a6c6f1e
Sumandeep Kaur <sumandeep4300@gmail.com>
2025-03-11 02:54:03 -0400

    Initial set up

---
+++ src/main/java/com/fincred/backend/FincredBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fincred.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincredBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincredBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fincred/backend/FincredBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fincred.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincredBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
